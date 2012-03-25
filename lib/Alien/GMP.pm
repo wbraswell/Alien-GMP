94264178988a433be3649bbbeb72d097a6f005d0
Richard Simões <rsimoes@cpan.org>
2012-03-25 14:16:29 -0500

    removed unused dependencies, added implicitly used ones

--- lib/Alien/GMP.pm
+++ lib/Alien/GMP.pm
@@ -4,8 +4,6 @@ use v5.10;
 use strict;
 use warnings FATAL => "all";
 use utf8;
-use File::Spec::Functions qw(catdir);
-use File::ShareDir "dist_dir";
 
 # VERSION
 # ABSTRACT: Build and install the GNU Multiple Precision library
