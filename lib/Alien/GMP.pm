87b383c06a20ba521a94c4b4c6217309c207f117
Richard Simões <rsimoes@cpan.org>
2012-04-02 03:45:27 -0500

    fix tests, fix populating of MANIFEST

--- lib/Alien/GMP.pm
+++ lib/Alien/GMP.pm
@@ -1,6 +1,5 @@
 package Alien::GMP;
 
-use v5.10;
 use strict;
 use warnings FATAL => "all";
 use utf8;
@@ -8,10 +7,8 @@ use utf8;
 # VERSION
 # ABSTRACT: Build and install the GNU Multiple Precision library
 
-sub inc_dir () {
-	return ## }
+sub inc_dir () { "##" }
 
-sub lib_dir () {
-	return ## }
+sub lib_dir () { "##" }
 
 1;
