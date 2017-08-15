2dbb2386e8c46ea26fcdaa03bbc3e39206712166
Graham Ollis <plicease@cpan.org>
2017-08-15 10:15:02 -0400

    get this for free in AB now

--- lib/Alien/GMP.pm
+++ lib/Alien/GMP.pm
@@ -8,18 +8,4 @@ use parent 'Alien::Base';
 # VERSION
 # ABSTRACT: Alien package for the GNU Multiple Precision library.
 
-sub Inline {
-	return unless $_[-1] eq 'C'; # Inline's error message is good
-	my $self = __PACKAGE__->new;
-	+{
-		LIBS => $self->libs,
-		INC => $self->cflags,
-		# NOTE: it may be necessary to also include
-		# <stdio.h>, <stdarg.h>, or <obstack.h> before gmp.h for
-		# certain functionality
-		#AUTO_INCLUDE => '#include "gmp.h"',
-	};
-}
-
-1;
 1;
